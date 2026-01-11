e810a375946f774f3ea47cc444dcc47679a6bcfb
JoaoFonsecaSC <fonsecajvictor@gmail.com>
2026-01-11 17:07:41 -0300

    fix: address extraction logic and add multi-page pagination

--- main.py
+++ main.py
@@ -3,49 +3,66 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
 import time
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+actions=ActionChains(driver)
 wait = WebDriverWait(driver, 10)
-driver.get('https://www.vivareal.com.br/aluguel/santa-catarina/sao-jose/apartamento_residencial/?transacao=aluguel&onde=%2CSanta+Catarina%2CS%C3%A3o+Jos%C3%A9%2C%2C%2C%2C%2Ccity%2CBR%3ESanta+Catarina%3ENULL%3ESao+Jose%2C-27.59504%2C-48.613768%2C&tipos=apartamento_residencial')
+
+driver.get('https://www.vivareal.com.br/aluguel/santa-catarina/sao-jose/apartamento_residencial/?transacao=aluguel&onde=%2CSanta+Catarina%2CS%C3%A3o+Jos%C3%A9%2C%2C%2C%2C%2Ccity%2CBR%3ESanta+Catarina%3ENULL%3ESao+Jose%2C-27.59504%2C-48.613768%2C&tipos=apartamento_residencial&page=1')
+
+def wait_time():
+    time.sleep(5)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(3) 
+    driver.execute_script("window.scrollTo(0, 0);")
+    time.sleep(2)
 
 
 titles, addresses, measures, prices, condo_prices, taxes, links = [], [], [], [], [], [], []
 
+def get_data():
+    cards = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'li[data-cy="rp-property-cd"]')))
 
-time.sleep(5)
-driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-time.sleep(3) 
-driver.execute_script("window.scrollTo(0, 0);")
-time.sleep(2)
+    for card in cards:
+        titles.append(card.find_element(By.CSS_SELECTOR, '[data-cy="rp-cardProperty-location-txt"]').text.split(" em ")[-0])
 
-cards = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'li[data-cy="rp-property-cd"]')))
+        addresses.append(card.find_element(By.CSS_SELECTOR, 'a').get_attribute('title').split(" em ")[-1].strip())
+        
+        measures.append(card.find_element(By.CSS_SELECTOR, '[data-cy="rp-cardProperty-propertyArea-txt"]').text)
+        
+        links.append(card.find_element(By.CSS_SELECTOR, 'a').get_attribute("href"))
+        
+        price_info = card.find_element(By.CSS_SELECTOR, '[data-cy="rp-cardProperty-price-txt"]').text
+        
+        prices.append(price_info.split("/")[0].replace("Aluguel de ", "").strip())
+        
+        condo = price_info.split("Cond. R$ ")[1].split("\n")[0].split(" ")[0] if "Cond. R$ " in price_info else "0"
+        condo_prices.append(condo)
+        
+
+        tax = price_info.split("IPTU R$ ")[1].split("\n")[0].split(" ")[0] if "IPTU R$ " in price_info else "0"
+        taxes.append(tax)
 
 
-for card in cards:
-    location_text = card.find_element(By.CSS_SELECTOR, '[data-cy="rp-cardProperty-location-txt"]').text
-    titles.append(location_text)
-    addresses.append(location_text.split(" em ")[-1])
+total_pages=int(driver.find_elements(By.CLASS_NAME,'olx-core-pagination__button')[-1].text)
+
+
+for page in range(total_pages):
+        btn_next = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'a[aria-label="próxima página"]')))
+        driver.execute_script("arguments[0].scrollIntoView();", btn_next)          
+        get_data()
+
+        if page<total_pages-1:
+            btn_next.click()
+            time.sleep(2)
+        else:
+             break
+
     
-    measures.append(card.find_element(By.CSS_SELECTOR, '[data-cy="rp-cardProperty-propertyArea-txt"]').text)
-    
-    links.append(card.find_element(By.CSS_SELECTOR, 'a').get_attribute("href"))
-    
-    price_info = card.find_element(By.CSS_SELECTOR, '[data-cy="rp-cardProperty-price-txt"]').text
-    
-    prices.append(price_info.split("/")[0].replace("Aluguel de ", "").strip())
-    
-    condo = price_info.split("Cond. R$ ")[1].split("\n")[0].split(" ")[0] if "Cond. R$ " in price_info else "0"
-    condo_prices.append(condo)
-    
-
-    tax = price_info.split("IPTU R$ ")[1].split("\n")[0].split(" ")[0] if "IPTU R$ " in price_info else "0"
-    taxes.append(tax)
-
-
-
 df_rents_info = pd.DataFrame({
         'Title': titles,
         'Area': measures,
